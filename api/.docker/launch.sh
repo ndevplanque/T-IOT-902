a9809256b0a5b4242a8fe20c60537adc33e54b7b
nicolasdelplanquechargemap <nicolas.delplanque@chargemap.com>
2025-02-14 12:01:47 +0100

    feat(api): added initial config

---
+++ api/.docker/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+cd "$DIR" || exit
+
+cp ../.env.srv ../.env
+export APPLICATION_NAME="t-iot-902-api"
+docker compose down --remove-orphans
+docker compose up "$@" --detach
+docker container exec -it "$APPLICATION_NAME" npm install
+echo "Launched $APPLICATION_NAME"
---
+++ api/README.md
@@ -0,0 +1,16 @@
+# T-IOT-902 API Documentation
+
+To run, use the following command:
+
+```bash
+sh .docker/launch.sh
+```
+
+This will start the server on port 3000.
+
+## Endpoints
+
+### GET /api/v1/health
+
+Returns the health of the server.
+
